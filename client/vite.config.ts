871d1a44322cd7f8010693ab206a0780fd0219a5
moisgames <VLAD981010@gmail.com>
2024-10-28 20:40:11 +0700

    chore Create React-Vite-TS App. And install Material UI, styled components.

---
+++ client/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/express_backend': "http://localhost:5000"
+    },
+  }
+})
